0d10e185a311fdd7cd6b651436b2f57d40b90069
GitHub utawareikka587@gmail.com <ec2-user@ip-172-31-35-138.ec2.internal>
2021-03-16 10:05:21 +0000

    [Start]5-3

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'e095055c1a68cbff9cb6f8eaf357d5298a4edc1f9cdeab7f6063477a96d664f351237deffc146e175e7c0a308cc30461704ce1d3a9aab5739d00ed99a541c14b'
